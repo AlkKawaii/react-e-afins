6bbf9ce2e9391787a8ca3bca33e1a407030ed99f
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-08-03 23:39:57 -0300

    versionamento de codigo kkj

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,16 @@
 import styles from './NotFound.module.css';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function NotFound() {
 	return (
-		<section className={styles.container}>
-            <h2>Opa, isso aqui não foi encontrado!</h2>
-            <span> :( </span>
-		</section>
+		<>
+			<Header />
+			<section className={styles.container}>
+				<h2>Opa, isso aqui não foi encontrado!</h2>
+				<span className={styles.sadFace}> :( </span>
+			</section>
+			<Footer />
+		</>
 	);
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Random from './pages/Random';
+import NotFound from './pages/NotFound';
 
 export default function AppRoutes() {
 	return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/random" element={<Random />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
