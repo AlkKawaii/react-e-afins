09c92f424190b48904a38e57a689455242567e40
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-08-11 16:58:09 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,10 +4,7 @@ import styles from './Card.module.css';
 export default function Card({ element }) {
 	return (
 		<article className={styles.card} title={element.title}>
-			<Link
-				to={`/random/${element.id}`}
-				target="_blank"
-				rel="noreferrer noopener">
+			<Link to={`/random/${element.id}`} rel="noreferrer noopener">
 				<img
 					src={`https://picsum.photos/1280/720?random=${Math.random()}&blur`}
 					alt="thumb"
