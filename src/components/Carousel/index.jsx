6bed02d099c4ef8bd28cf5c290292e83e2cf3793
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-08-16 07:31:18 -0300

    Update index.jsx

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,24 +1,24 @@
-import { Swiper } from 'swiper/react';
-import { Navigation, Mousewheel, FreeMode } from 'swiper/modules';
-import './Carousel.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
+import { Swiper } from "swiper/react";
+import { Mousewheel, FreeMode } from "swiper/modules";
+import "./Carousel.css";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function Carousel({ children }) {
-	return (
-		<div className="carousel">
-			<Swiper
-				navigation={true}
-				mousewheel={true}
-				freeMode={true}
-				modules={[Navigation, Mousewheel, FreeMode]}
-				direction="horizontal"
-				loop={true}
-				grabCursor={true}
-				spaceBetween={3}
-				slidesPerView={4.85}>
-				{children}
-			</Swiper>
-		</div>
-	);
+  return (
+    <div className="carousel">
+      <Swiper
+        mousewheel={true}
+        freeMode={true}
+        modules={[Mousewheel, FreeMode]}
+        direction="horizontal"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={100}
+        slidesPerView={5.5}
+      >
+        {children}
+      </Swiper>
+    </div>
+  );
 }
