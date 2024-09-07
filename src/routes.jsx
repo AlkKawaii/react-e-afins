de1f38b357ba34c379f73a3fcd8b36b8a0fc579d
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-09-07 13:33:16 -0300

    changed BrowserRouter to Router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Random from './pages/Random';
 import NotFound from './pages/NotFound';
@@ -6,13 +6,13 @@ import Search from './pages/Search';
 
 export default function AppRoutes() {
 	return (
-		<BrowserRouter>
+		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/random/:id" element={<Random />} />
 				<Route path="/search" element={<Search />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
-		</BrowserRouter>
+		</Router>
 	);
 }
